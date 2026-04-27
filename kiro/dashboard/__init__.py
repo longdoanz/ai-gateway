512e66d94f5957673fe2094c835f6fe6936e5699
LongDT <longdt95@gmail.com>
2026-04-27 11:58:40 +0700

    feat: register analytics router in dashboard

--- kiro/dashboard/__init__.py
+++ kiro/dashboard/__init__.py
@@ -6,6 +6,7 @@ from kiro.dashboard.routes_keys import router as keys_router
 from kiro.dashboard.routes_overview import router as overview_router
 from kiro.dashboard.routes_config import router as config_router
 from kiro.dashboard.routes_import import router as import_router
+from kiro.dashboard.routes_analytics import router as analytics_router
 
 dashboard_router = APIRouter(prefix="/api", tags=["dashboard"])
 dashboard_router.include_router(auth_router)
@@ -14,3 +15,4 @@ dashboard_router.include_router(keys_router)
 dashboard_router.include_router(overview_router)
 dashboard_router.include_router(config_router)
 dashboard_router.include_router(import_router)
+dashboard_router.include_router(analytics_router)
